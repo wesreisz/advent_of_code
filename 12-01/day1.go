ba343389756c9ebeb5a3cf3e177b9304f1be6ff9
Wesley Reisz <wes@wesleyreisz.com>
2021-12-01 09:13:07 -0500

    initial commit'

---
+++ 12-01/day1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./input-day1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Advent of Code 2021
+Using this Advent of Code to focus on using Golang.
+
