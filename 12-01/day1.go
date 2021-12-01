26e95bb4e7c5b1edb897ac856affd6e3e0c1c4a9
Wesley Reisz <wes@wesleyreisz.com>
2021-12-01 10:34:12 -0500

    day 1, part 1 solution

--- 12-01/day1.go
+++ 12-01/day1.go
@@ -5,19 +5,50 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	file, err := os.Open("./input-day1.txt")
+	depth_increases := 0
+	depth_splice := loadfile("./input-day1.txt")
+	previous_depth := int64(0)
+
+	//fmt.Println(previous_depth)
+
+	for current_pos, current_depth := range depth_splice {
+		if current_pos <= 0 {
+			continue
+		}
+		previous_depth = depth_splice[current_pos-1]
+		if current_depth > previous_depth {
+			depth_increases += 1
+		}
+	}
+
+	fmt.Println("Depth Increases: ", depth_increases)
+}
+
+func loadfile(file_name string) []int64 {
+	depths := make([]int64, 0)
+
+	file, err := os.Open(file_name)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
+	i := 0
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		input_num, err2 := strconv.ParseInt(scanner.Text(), 10, 64)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		depths = append(depths, input_num)
+
+		i += 1
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	return depths
 }
